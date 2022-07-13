49d2d6bad743c0b3b95bfb4b8093e985a36a9b0f
Manish kumar <91480902+manni2000@users.noreply.github.com>
2022-07-13 11:03:03 +0530

    hbd
    mdm

---
+++ index.js
@@ -0,0 +1,58 @@
+var MusicLister= document.querySelectorAll(".Music").length;
+
+for(var i=0; i<MusicLister;i++){
+  document.querySelectorAll(".Music")[i].addEventListener("click", function(){
+
+    this.style.color="black";
+
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+  });
+}
+
+    document.addEventListener("keypress", function(event){
+      makeSound(event.key);
+    });
+
+    function makeSound(key){
+      switch(key){
+        case "a":
+          var first = new Audio("music/Guitar1.wav");
+          first.play();
+          break;
+  
+        case "b" :
+          var second = new Audio("music/piano1.wav");
+          second.play();
+          break;
+  
+        case "c":
+          var third= new Audio("music/drum.wav");
+          third.play();
+          break;
+  
+        case "d":
+          var fouth = new Audio("music/violin.mp3");
+          fouth.play();
+          break;
+        
+        case "e":
+          var fifty = new Audio("music/Sitar1.mp3");
+          fifty.play();
+          break;
+  
+        case "f":
+          var sixth = new Audio("music/flute.wav");
+          sixth.play();
+          break;
+  
+        case "g":
+          var seventh = new Audio("music/drum3.wav");
+          seventh.play();
+          break;
+  
+          default: console.log(buttonInnerHTML);
+      }
+   };
+    
